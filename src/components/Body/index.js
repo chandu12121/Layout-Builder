6e7e304012e107f8ccaa66bd5d3861f7a5b8fff8
chandrashekar <chandrashekarp7769@gmail.com>
2024-04-22 15:32:48 +0000

    commit message

---
+++ src/components/Body/index.js
@@ -0,0 +1,48 @@
+// Write your code here
+import ConfigurationContext from '../../context/ConfigurationContext'
+import './index.css'
+
+const Body = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {showContent, showRightNavbar, showLeftNavbar} = value
+
+      return (
+        <div className="body-container">
+          {showLeftNavbar ? (
+            <div className="left-navbar">
+              <h1>Left Navbar Menu</h1>
+              <ul className="list-items">
+                <li className="item">Item 1</li>
+                <li className="item">Item 2</li>
+                <li className="item">Item 3</li>
+                <li className="item">Item 4</li>
+              </ul>
+            </div>
+          ) : null}
+          {showContent ? (
+            <div className="content">
+              <h1>Content</h1>
+              <p>
+                Lorem ipsum dolor amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aligue. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+          ) : null}
+          {showRightNavbar ? (
+            <div className="right-navbar">
+              <h1>Right Navbar</h1>
+              <div className="ad-boc-container">
+                <p>Ad 1</p>
+                <p>Ad 2</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+
+export default Body
